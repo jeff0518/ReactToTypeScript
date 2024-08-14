6f16411590c2dd64b3e6e34eaa3caf6e73dac14f
jeff0518 <cycivs07@icloud.com>
2024-08-14 21:56:18 +0800

    feat: add components

---
+++ Project1/src/CoreConcept.tsx
@@ -0,0 +1,22 @@
+import "./index.css";
+
+export interface CoreConceptProps {
+  title: string;
+  description: string;
+  image: string;
+}
+
+function CoreConcept(props: CoreConceptProps) {
+  const { title, description, image } = props;
+  return (
+    <>
+      <li>
+        <img src={image} alt="image" />
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </li>
+    </>
+  );
+}
+
+export default CoreConcept;
